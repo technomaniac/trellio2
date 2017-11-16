dcb94ef4b9329e50499a0e560be18c17533588d1
abhishek <abhishek@magicpin.in>
2017-11-16 17:57:35 +0530

    updated readme

--- README.rst
+++ README.rst
@@ -1,8 +1,14 @@
-# README #
+Trellio2
+--------
+
 A high performance python asyncio driven http framework internally uses `sanic <https://github.com/channelcat/sanic/>`_
 
-### Language 
-* Python 3.6
 
-### Installation
+Installation
+------------
 * pip install trellio2
+
+
+Examples
+--------
+* Goto examples directory
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='trellio2',
     version='0.1',
-    packages=find_packages(exclude=['tests', 'example']),
+    packages=find_packages(exclude=['tests', 'examples']),
     url='https://bitbucket.org/samast/dataloader.git',
     author='Abhishek Verma',
     author_email='abhishek.verma@magicpin.in',
